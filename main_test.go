5bcf1ee107a11097b6cc7c32f4aa07913bf2fb4b
Bernardo Loureiro <bernardo.lou@gmail.com>
2018-04-12 01:27:55 -0300

    Updating bench tests

--- main_test.go
+++ main_test.go
@@ -2,8 +2,19 @@ package main
 
 import "testing"
 
-func BenchmarkDo(b *testing.B) {
-	for n := 0; n < 50; n++ {
+func benchmarkDo(i int, b *testing.B) {
+	for n := 0; n < 100; n++ {
 		do()
 	}
 }
+
+func BenchmarkDo1(b *testing.B)           { benchmarkDo(1, b) }
+func BenchmarkDo100(b *testing.B)         { benchmarkDo(100, b) }
+func BenchmarkDo1000(b *testing.B)        { benchmarkDo(1000, b) }
+func BenchmarkDo10000(b *testing.B)       { benchmarkDo(10000, b) }
+func BenchmarkDo100000(b *testing.B)      { benchmarkDo(100000, b) }
+func BenchmarkDo1000000(b *testing.B)     { benchmarkDo(1000000, b) }
+func BenchmarkDo10000000(b *testing.B)    { benchmarkDo(10000000, b) }
+func BenchmarkDo100000000(b *testing.B)   { benchmarkDo(100000000, b) }
+func BenchmarkDo1000000000(b *testing.B)  { benchmarkDo(1000000000, b) }
+func BenchmarkDo10000000000(b *testing.B) { benchmarkDo(10000000000, b) }
